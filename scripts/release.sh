cf2e7ba8f6ddb9861b5241710b3619190bffb2ec
ikaio <ikaiodev@gmail.com>
2024-02-03 03:00:54 +0000

    fix: version var asign at build

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ version="0.1.$(($commit_count + 1))"
 echo "Creating $version release."
 
 mkdir release 2>/dev/null
-go build -ldflags="-X help.Version=$version" -o ./release/
+go build -ldflags="-X github.com/ikaio/tailmplx/help.Version=$version" -o ./release/
 
 cp -r web/public release
 tailwindcss --minify -c web/tailwind.config.js -i web/input.css -o release/public/assets/output.css
